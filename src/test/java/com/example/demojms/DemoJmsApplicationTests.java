f541b290f9b05066e97e62222f62dce5c8c33ba9
om <ovimoldo9@gmail.com>
2021-11-22 22:49:11 -0500

    initial commit

---
+++ src/test/java/com/example/demojms/DemoJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
